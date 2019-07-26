409fa5bab96a579cd82741406bb1ec21c9a3a58e
pramodya <pramodyaabeysinghe@gmail.com>
2019-07-26 22:16:55 +0530

    main functionalities of a calculator

--- Calc/App.js
+++ Calc/App.js
@@ -10,43 +10,105 @@ import React, {Component} from 'react';
 import {Button, StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 
 export default class App extends Component<> {
+
+  constructor() {
+    super();
+    this.state = {
+      resultText: "",
+      calculatedText: "0"
+    };
+    this.operations = ['DEL','+','-','*','/'];
+  }
+
+  calculateResult() {
+    const text = this.state.resultText;
+    this.setState({
+      calculatedText: eval(text)
+    })
+  }
+
+  operate(operand) {
+    switch (operand) {
+      case 'DEL':
+        let text = this.state.resultText.split('');
+        text.pop();
+        this.setState({
+          resultText: text.join('')
+        });
+        break;
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        const lastChar = this.state.resultText.split('').pop();
+        
+        if (this.operations.indexOf(lastChar) > 0) return;
+
+        if (this.state.resultText === '') return;
+        this.setState({
+          resultText: this.state.resultText + operand
+        })
+
+    }
+  }
+
+  validateText() {
+    let text = this.state.resultText;
+
+    return this.operations.indexOf(text.split('').pop()) <= 0;
+  }
+
+  buttonPressed(text) {
+    if (text === '=') {
+      return this.validateText() && this.calculateResult()
+    }
+    this.setState({
+      resultText: this.state.resultText+text
+    })
+  }
+
   render() {
     let rows = [];
-    for (let i=0; i<3; i++){
+    let nums = [[1,2,3], [4,5,6], [7,8,9], ['.',0,'=']];
+    
+    for (let i=0; i<4; i++) {
       let row = [];
       for (let j=0; j<3; j++) {
         row.push(
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>{j+i*3+1}</Text>
-            </TouchableOpacity>)
+            <TouchableOpacity key={nums[i][j]} onPress={() => this.buttonPressed(nums[i][j])}>
+              <Text style={styles.buttonText}>{nums[i][j]}</Text>
+            </TouchableOpacity>
+        )
       }
-      rows.push(<View style={styles.row}>{row}</View>)
+      rows.push(
+          <View key={i} style={styles.row}>{row}</View>
+      )
+    }
+
+    let ops = [];
+
+    for (let i=0; i<5; i++) {
+      ops.push(
+          <TouchableOpacity key={this.operations[i]} onPress={() => this.operate(this.operations[i])} style={styles.button}>
+            <Text style={[styles.buttonText, styles.white]}>{this.operations[i]}</Text>
+          </TouchableOpacity>
+      )
     }
 
     return (
       <View style={styles.container}>
         <View style={styles.calculation}>
-          <Text style={styles.calculationText}> 10,000/100</Text>
+          <Text style={styles.calculationText}> {this.state.resultText}</Text>
         </View>
         <View style={styles.result}>
-          <Text style={styles.resultText}>100</Text>
+          <Text style={styles.resultText}>{this.state.calculatedText}</Text>
         </View>
         <View style={styles.buttons}>
           <View style={styles.numbers}>
             {rows}
-            <View style={styles.row}>
-              <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>+/-</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>0</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>.</Text></TouchableOpacity>
-            </View>
-
           </View>
           <View style={styles.operations}>
-            <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>=</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>+</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>-</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>X</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>/</Text></TouchableOpacity>
+            {ops}
           </View>
         </View>
       </View>
